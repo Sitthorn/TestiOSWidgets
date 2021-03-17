9abe9a93bef46b0206246fd452a21c4fe9f61ba9
Sitthorn Ch <sitthorn.chou@gmail.com>
2021-03-17 22:53:17 +0700

    Initial Commit

--- TestiOSWidgets/ViewController.swift
+++ TestiOSWidgets/ViewController.swift
@@ -1,19 +1,41 @@
 //
 //  ViewController.swift
-//  TestiOSWidgets
+//  ComponentTest
 //
-//  Created by Sitthorn Ch on 17/3/2564 BE.
+//  Created by SIDDHANT-TCS on 16/2/2564 BE.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, TTBBaseBlueDesign {
 
+    @IBOutlet var buttons: [UIButton]!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setTitleNavigation(title: "Components Labs")
+        setupLayout()
     }
 
+    @IBAction func enableHandler(_ sender: UISegmentedControl) {
+        let enable = (sender.selectedSegmentIndex == 0)
+    
+        for button in buttons {
+            button.isEnabled = enable
+        }
+    }
+    
+   
+    
+}
 
+enum TestError: PrimaryError {
+    case mismatch
+    
+    var description: String? {
+        return "Please Input valid data"
+    }
 }
 
